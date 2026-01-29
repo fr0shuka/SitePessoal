726021e5a59e6519853d601c6c636a72a7454f2a
MiguelBorges <1231548@isep.ipp.pt>
2026-01-29 15:00:05 +0000

    vamos

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Documentação
           </a>
         </div>
       </main>
